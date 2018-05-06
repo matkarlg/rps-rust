5095c7f91b900c29119c2e28b317fe07755ade52
Mathias Karlgren <mathias.karlgren@gmail.com>
2018-05-06 20:23:09 +0200

    Fix compile error on stable

--- src/rps/mod.rs
+++ src/rps/mod.rs
@@ -8,9 +8,9 @@ pub fn play_hands(p1: &Hand, p2: &Hand) -> GameResult {
     use rps::Hand::*;
 
     match (p1, p2) {
-        (Paper, Rock) => Won,
-        (Scissors, Paper) => Won,
-        (Rock, Scissors) => Won,
+        (&Paper, &Rock) => Won,
+        (&Scissors, &Paper) => Won,
+        (&Rock, &Scissors) => Won,
         _ if p1 == p2 => Draw,
         _ => Lost,
     }
